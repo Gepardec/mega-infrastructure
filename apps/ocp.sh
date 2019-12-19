f4bcd9ed5d5237471492c1976c9cb7056cd49afa
Thomas <thomas.herzog@gepardec.com>
2019-12-19 10:09:51 +0100

    - Fixed invalid path to maven-pvc.yaml

--- apps/ocp.sh
+++ apps/ocp.sh
@@ -7,13 +7,13 @@ set -u
 function createJenkins {
     oc process -f jenkins/jenkins-bc.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc apply -f -
     oc process -f jenkins/jenkins.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc apply -f -
-    oc create -f  jenkions/maven-pvc.yaml
+    oc create -f  jenkins/maven-pvc.yaml
 }
 
 function deleteJenkins {
     oc process -f jenkins/jenkins-bc.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc delete -f -
     oc process -f jenkins/jenkins.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc delete -f -
-    oc delete -f  jenkions/maven-pvc.yaml
+    oc delete -f  jenkins/maven-pvc.yaml
 }
 
 function recreateJenkins {
