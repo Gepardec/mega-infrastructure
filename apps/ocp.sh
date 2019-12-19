eb838728d261b8333266c5b4c95339af73c0656d
Thomas <thomas.herzog@gepardec.com>
2019-12-19 09:55:25 +0100

    - Added mega infrastructure sources

---
+++ apps/ocp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd $(pwd)
+
+set -u
+
+function createJenkins {
+    oc process -f jenkins/jenkins-bc.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc apply -f -
+    oc process -f jenkins/jenkins.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc apply -f -
+    oc create -f  jenkions/maven-pvc.yaml
+}
+
+function deleteJenkins {
+    oc process -f jenkins/jenkins-bc.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc delete -f -
+    oc process -f jenkins/jenkins.yaml -o yaml --param-file=jenkins/jenkins.properties --ignore-unknown-parameters=true | oc delete -f -
+    oc delete -f  jenkions/maven-pvc.yaml
+}
+
+function recreateJenkins {
+    deleteJenkins
+    createJenkins
+}
+
+${1}
