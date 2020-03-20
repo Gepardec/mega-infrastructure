4e24a9b48ab3775808e9d966934f297b880b389f
Thomas <thomas.herzog@gepardec.com>
2020-03-20 09:32:57 +0100

    - Switched back to jenkins slave base - Modified bc yamls - Trick with passwd did not work - Updated jenkins plugins.txt - removed new build job - removed -old suffix of job

--- docker/agent-jdk/docker-entrypoint.sh
+++ docker/agent-jdk/docker-entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Set current user in nss_wrapper
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
-
-if [ "$(id -u)" != "0" ]; then
-    echo "Setting user ${USER_ID}:${GROUP_ID} to /etc/passwd and /etc/group"
-    echo "default:x:${USER_ID}:${GROUP_ID}:Default Application User:${HOME}:/sbin/nologin" >> /etc/passwd
-    echo "${USER_ID}:x:${GROUP_ID}:" >> /etc/group
-fi
 
 $@
